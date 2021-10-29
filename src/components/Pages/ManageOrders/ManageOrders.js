4a1449274acae2f88bde2e4261f8175bb705e5ea
Ishmamur Rahman <trnwrckd@gmail.com>
2021-10-29 07:34:54 +0600

    functional requirements met ig

--- src/components/Pages/ManageOrders/ManageOrders.js
+++ src/components/Pages/ManageOrders/ManageOrders.js
@@ -2,23 +2,70 @@ import './ManageOrders.css';
 
 import React, { useEffect, useState } from 'react';
 import Order from '../../Shared/Order/Order';
+import { Handler } from 'leaflet';
 
 const ManageOrders = () => {
 
     const [orders, setOrders] = useState([]);
 
+    const [order, setOrder] = useState({});
+
     useEffect(() => {
         fetch(`http://localhost:5000/orders/`)
             .then(res => res.json())
             .then(data => setOrders(data));
-    }, [])
+    }, [order]);
+
+    const handleDeleteOrder = (id) => {
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/orders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingOrders = orders.filter(order => order._id !== id);
+                        setOrders(remainingOrders);
+                    }
+                });
+        }
+    }
+
+    const handleUpdateStatus = (id) => {
+        
+        const updatedOrder = { ...order, 'orderStatus': 'confirmed' };
+        setOrder(updatedOrder);
+
+        const url = `http://localhost:5000/orders/${id}`;
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('Update Successful');
+                    setOrder(data);
+                }
+            })
+    }
 
     return (
         <div className =" container mt-nav">
             <h1>Manage Orders</h1>
             <div>
                 {
-                    orders.map(order=> <Order key ={order._id} order = {order}></Order>)
+                    orders.map(order => <Order key={order._id} order={order}>
+                        <button className="btn-danger" onClick={()=> handleDeleteOrder(order._id)}> Delete</button>
+                        <button className="btn-success" onClick={()=>{handleUpdateStatus(order._id)}}> Update status</button>
+                    </Order>)
                 }
             </div>
         </div>
--- src/components/Shared/Order/Order.js
+++ src/components/Shared/Order/Order.js
@@ -1,16 +1,24 @@
 import './Order.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 const Order = (props) =>{
 
-    const {address, name, date}= props.order;
-    
+    const {address, name, date , orderStatus}= props.order;
+    // const [status, setStatus] = useState('pending');
+
+    // useEffect(() => {
+    //     setStatus(orderStatus)
+    // }, [orderStatus]);
+
     return (
         <div className="bg-dark m-3 text-light">
             <h3>{address}</h3>
             <h3>{name}</h3>
             <h3>{date}</h3>
+            <h3>{orderStatus}</h3>
+            <div>{props.children}</div>
         </div>
     );
 };
