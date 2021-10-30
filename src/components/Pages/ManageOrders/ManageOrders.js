cab50b153fe88b2a9a3ba7987c5966fd6e4b8541
Ishmamur Rahman <trnwrckd@gmail.com>
2021-10-30 18:17:36 +0600

    added loader on all pages

--- src/components/Pages/ManageOrders/ManageOrders.js
+++ src/components/Pages/ManageOrders/ManageOrders.js
@@ -2,19 +2,25 @@ import './ManageOrders.css';
 
 import React, { useEffect, useState } from 'react';
 import Order from '../../Shared/Order/Order';
+import Loader from '../../Shared/Loader/Loader';
 
 const ManageOrders = () => {
 
     const [orders, setOrders] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
     const [order, setOrder] = useState({});
 
     useEffect(() => {
         fetch(`https://enigmatic-caverns-80998.herokuapp.com/orders/`)
             .then(res => res.json())
-            .then(data => setOrders(data));
+            .then(data => {
+                setOrders(data);
+                setLoaded(true);
+            });
     }, [order]);
 
+    // delete order
     const handleDeleteOrder = (id) => {
         const proceed = window.confirm('Are you sure, you want to delete?');
 
@@ -34,6 +40,7 @@ const ManageOrders = () => {
         }
     }
 
+    // update order status
     const handleUpdateStatus = (id) => {
         
         const updatedOrder = { ...order, 'orderStatus': 'confirmed' };
@@ -57,35 +64,38 @@ const ManageOrders = () => {
             })
     }
 
-    if (orders.length === 0) {
-        return (
-            <div className="mt-nav container py-5 my-5">
-                 <h2 className="py-5 my-5">No Current Orders</h2>
-            </div>
-        );
-    }
-
+    if (!loaded) return <Loader />
     else {
-        return (
-            <div className ="container mt-nav py-5 my-5">
-                <h1>Manage Orders</h1>
-                <div className="py-2 my-2 row row-cols-1 row-cols-md-2 g-5">
-                    {
-                        orders.map(order => <Order key={order._id} order={order}>
-                            <div className="d-flex justify-content-evenly">
-                                <button className="btn btn-danger" onClick={() => handleDeleteOrder(order._id)}> Delete
-                                    <i class="fas fa-trash-alt ms-1"></i>
-                                </button>
-                                <button className="btn btn-success" onClick={()=>{handleUpdateStatus(order._id)}}> 
-                                    Update status
-                                    <i class="far fa-check-circle ms-1"></i>
-                                </button>
-                            </div>
-                        </Order>)
-                    }
+        if (orders.length === 0) {
+            return (
+                <div className="mt-nav container py-5 my-5">
+                    <h2 className="py-5 my-5">No Current Orders</h2>
                 </div>
-            </div>
-        );
+            );
+        }
+
+        else {
+            return (
+                <div className ="container mt-nav py-5 my-5">
+                    <h1>Manage Orders</h1>
+                    <div className="py-2 my-2 row row-cols-1 row-cols-md-2 g-5">
+                        {
+                            orders.map(order => <Order key={order._id} order={order}>
+                                <div className="d-flex justify-content-evenly">
+                                    <button className="btn btn-danger" onClick={() => handleDeleteOrder(order._id)}> Delete
+                                        <i className="fas fa-trash-alt ms-1"></i>
+                                    </button>
+                                    <button className="btn btn-success" onClick={()=>{handleUpdateStatus(order._id)}}> 
+                                        Update status
+                                        <i className="far fa-check-circle ms-1"></i>
+                                    </button>
+                                </div>
+                            </Order>)
+                        }
+                    </div>
+                </div>
+            );
+        }
     }
 };
 
--- src/components/Pages/MyOrders/MyOrders.js
+++ src/components/Pages/MyOrders/MyOrders.js
@@ -3,17 +3,22 @@ import './MyOrders.css';
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../../hooks/useAuth';
 import Order from '../../Shared/Order/Order';
+import Loader from '../../Shared/Loader/Loader';
 
 const MyOrders = () => {
 
     const { user } = useAuth();
 
     const [orders, setOrders] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         fetch(`https://enigmatic-caverns-80998.herokuapp.com/orders/${user.uid}`)
             .then(res => res.json())
-            .then(data => setOrders(data));
+            .then(data => {
+                setOrders(data);
+                setLoaded(true);
+            });
     }, [user.uid]);
 
      const handleDeleteOrder = (id) => {
@@ -34,31 +39,35 @@ const MyOrders = () => {
                 });
         }
     }
-    console.log(orders);
-    if (orders.length === 0) {
-        return (
-            <div className="mt-nav container py-5 my-5">
-                 <h2 className="py-5 my-5">No Current Orders</h2>
-            </div>
-        );
-    }
+    
+    if (!loaded) return <Loader />
     else {
-        return (
-            <div className="mt-nav container py-5 my-5">
-                <h2>{user.displayName}, Here are all your orders.</h2>
-                <div className="py-2 my-2 row row-cols-1 row-cols-md-2 g-5">
-                    {
-                        orders.map(order => <Order key={order._id} order={order}>
-                            <button className="btn btn-danger" onClick={() => handleDeleteOrder(order._id)}>
-                                Delete
-                                <i class="fas fa-trash-alt ms-1"></i>
-                            </button>
-                        </Order>)
-                    }
+        if (orders.length === 0) {
+            return (
+                <div className="mt-nav container py-5 my-5">
+                     <h2 className="py-5 my-5">No Current Orders</h2>
                 </div>
-            </div>
-        );
+            );
+        }
+        else {
+            return (
+                <div className="mt-nav container py-5 my-5">
+                    <h2>{user.displayName}, Here are all your orders.</h2>
+                    <div className="py-2 my-2 row row-cols-1 row-cols-md-2 g-5">
+                        {
+                            orders.map(order => <Order key={order._id} order={order}>
+                                <button className="btn btn-danger" onClick={() => handleDeleteOrder(order._id)}>
+                                    Delete
+                                    <i className="fas fa-trash-alt ms-1"></i>
+                                </button>
+                            </Order>)
+                        }
+                    </div>
+                </div>
+            );
+        }
     }
+
 };
 
 export default MyOrders;
